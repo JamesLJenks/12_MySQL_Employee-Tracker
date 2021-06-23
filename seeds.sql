01bbb74e3e036d064026e26a4ed239166690b751
Jay Jenks <jamesljenks@gmail.com>
2021-06-22 21:19:44 -0400

    Updated seeds.sql with values.

--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,12 @@
 USE businessDB;
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ()
-
--- 1, John, Doe, Sales Lead, Sales, 100000, Ashley Rodriguez
--- 2, Mike, Chan, Salesperson, Sales, 80000, John Doe
--- 3, Ashley, Rodriguez, Lead Engineer, Engineering, 150000, null
--- 4, Kevin, Tupik, Software Engineer, Engineering, 120000, Ashley Rodriguez
--- 5, Malia, Brown, Accountant, Finance, 125000, null
--- 6, Sarah, Lourd, Legal Team Lead, Legal, 250000, null
--- 7, Tom, Allen, Lawyer, Legal, 190000, Sarah Lourd
--- 8, Christian Eckenrode, Lead Engineer, Engineering, 150000, Mike Chan
--- 9, Tammer, Galal, Software Engineer, Engineering, 120000, Kevin Tupik
+VALUES ("John", "Doe", "Sales Lead", "Sales", 100000, "Ashley Rodriguez"),
+("Mike", "Chan", "Salesperson", "Sales", 80000, "John Doe"),
+("Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, null),
+("Kevin", "Tupik", "Software Engineer", "Engineering", 120000, "Ashley Rodriguez"),
+("Malia", "Brown", "Accountant", "Finance", 125000, null),
+("Sarah", "Lourd", "Legal Team Lead", "Legal", 250000, null),
+("Tom", "Allen", "Lawyer", "Legal", 190000, "Sarah Lourd"),
+("Christian", "Eckenrode", "Lead Engineer", "Engineering", 150000, "Mike Chan"),
+("Tammer", "Galal", "Software Engineer", "Engineering", 120000, "Kevin Tupik");
